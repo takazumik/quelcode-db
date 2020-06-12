ccce2a2fac61bbafbbac063f29c4c598e4585a21
Takazumi Kawakami <takazumikwkm@gmail.com>
2020-06-12 11:22:47 +0900

    一旦save

---
+++ db/db-challenge3/db-001/001.sql
@@ -0,0 +1,54 @@
+INSERT INTO users 
+VALUES (
+    1, 'admin@labot.inc', 'admin',	'admin', 'このスペースの管理者です。', '00-0000-0000', 0, 2020/04/20 15:47:52, 2020/04/20 15:47:52
+)
+
+INSERT INTO users 
+VALUES (
+    2, 'mysql@labot.inc', 'mysql55', 'mysql', '080-1234-1253', 0, 2020/04/22 16:02:55, 2020/04/22 16:02:55
+)
+
+INSERT INTO users 
+VALUES (
+    3, 'sqlserver@labot.inc', 'apple', 'sqlserver', 'オラクルデータベースが好きです。', 0, 2020/04/22 17:32:55, 2020/04/22 17:32:55
+)
+
+INSERT INTO users 
+VALUES (
+    4, 'mariadb@labot.inc', 'maria', 'mariadb', '03-6666-5879', 0, 2020/04/22 18:45:21, 2020/04/22 18:45:21
+)
+
+INSERT INTO users 
+VALUES (
+    5, 'laravel@labot.inc', 'lara', 'laravel', 0, 2020/04/22 18:46:01, 2020/04/22 18:46:01
+)
+
+INSERT INTO users 
+VALUES (
+    6, 'cakephp@labot.inc', 'cake89', 'cake', '03-987-1234', '090-5555-7854', 0, 2020/04/23 02:51:06, 2020/04/25 11:45:06
+)
+
+INSERT INTO users 
+VALUES (
+    7, 'react@labot.inc', 'node', 'react', 0, 2020/04/23 09:54:32, 2020/04/23 09:54:32
+)
+
+INSERT INTO users 
+VALUES (
+    8, 'html@labot.inc', 'css3', 'html5', '080-5849-5555', 0, 2020/04/23 09:55:14, 2020/04/23 09:55:14
+)
+
+INSERT INTO users 
+VALUES (
+    9, php@labot.inc, php7, php, 伝説のphp6, 0, 2020/04/24 01:01:06, 2020/04/24 01:01:06
+)
+
+INSERT INTO users 
+VALUES (
+    10, javascript@labot.inc, react, js, Hello world, 0120-555-458, 0, 2020/04/24 05:03:58, 2020/04/24 05:03:58
+)
+
+INSERT INTO users 
+VALUES (
+    11, microsoft@gmail.com, google, amazon, facebook, 080-9999-5555, 1, 2020/04/24 06:00:02, 2020/04/26 12:13:43
+)
