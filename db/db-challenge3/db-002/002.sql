0616469a8b06d6f4159e48f74dbf71063ae43aad
川上登玄 <takazumi@takazumi-pc.local>
2020-06-12 16:46:27 +0900

    seve

--- db/db-challenge3/db-002/002.sql
+++ db/db-challenge3/db-002/002.sql
@@ -1,39 +1,41 @@
-INSERT INTO chatoroom
+INSERT INTO chatrooms (
+    chatroom_id, chatroom_name,overview, file_is_valid, is_direct, is_deleted,author_id, changer_id, created_at,modified_at
+)
 (
     1, 'りんご', '利用者の集まり', 1, 0, 0, 1, 2020/04/21 11:26:32, 1, 2020/04/21 11:26:32
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     2, 'ごりら', 'sqlの集まり', 1, 0, 0, 2, 2020/04/23 15:42:05, 2, 2020/04/23 15:42:05
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     3, 'らっぱ', '言語の集まり', 1, 0, 0, 8, 2020/04/24 11:03:16, 9, 2020/04/25 10:33:55
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     4, 'ぱんだ', 'フレームワークの集まり', 0, 0, 0, 5, 2020/04/23 18:14:28, 7, 2020/04/24 20:33:06
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     5, 'ごま', 'adminとmysqlのダイレクトチャット', 1, 1, 0, 1, 2020/04/23 14:34:58, 1, 2020/04/23 14:34:58
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     6, 'まり', 'adminとsqlserverのダイレクトチャット', 0, 1, 0, 1, 2020/04/23 14:45:22, 1, 2020/04/23 14:45:22
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     7, 'りす', 'adminとjsのダイレクトチャット', 1, 1, 0, 1, 2020/04/24 10:55:21, 1, 2020/04/24 10:55:21
 )
 
-INSERT INTO chatoroom
+INSERT INTO chatrooms
 (
     8, 'かめ', 'adminとamazonのダイレクトチャット', 1, 1, 1, 1, 2020/04/24 11:02:20, 1, 2020/04/27 15:41:33
 )
---
+++ db/db-challenge3/db-003/003.sql
@@ -0,0 +1,196 @@
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    1, 1, 2020/04/21 11:26:32
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    2, 1, 2020/04/22 16:02:55
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    3, 1, 2020/04/22 17:32:55
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    4, 1, 2020/04/22 18:45:21
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    5, 1, 2020/04/22 18:46:01    
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    6, 1, 2020/04/23 02:51:06
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    7, 1, 2020/04/23 09:54:32
+)
+
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    8, 1, 2020/04/23 09:55:14
+)
+
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    9, 1, 2020/04/24 01:01:06
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    10, 1, 2020/04/24 05:03:58
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    11, 1, 2020/04/24 06:00:02
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    2, 2, 2020/04/23 15:42:05
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    3, 2, 2020/04/23 18:22:10
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    4, 2, 2020/04/23 20:14:30
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    8, 3, 2020/04/24 11:03:16
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    9, 3, 2020/04/24 15:08:44
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    10, 3, 2020/04/24 18:01:06
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    5, 4, 2020/04/23 18:14:28
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    6, 4, 2020/04/24 08:24:38
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    7, 4, 2020/04/24 10:11:15
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    1, 5, 2020/04/23 14:34:58
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    2, 5, 2020/04/23 14:34:58
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    1, 6, 2020/04/23 14:45:22
+)
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    3, 6, 2020/04/23 14:45:22
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    1, 7, 2020/04/24 10:55:21
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    10, 7, 2020/04/24 10:55:21
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    1, 8, 2020/04/24 11:02:20
+)
+
+INSERT INTO users_chatrooms (
+    user_id, chatroom_id, participate_at   
+)
+VALUES (
+    11, 8, 2020/04/24 11:02:20
+)
