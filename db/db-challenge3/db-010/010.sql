b7e4765ec0adbeda966ba40158a0fbef8bd8b66a
川上登玄 <takazumi@takazumi-pc.local>
2020-06-16 18:29:16 +0900

    db-010を修正しました

--- db/db-challenge3/db-010/010.sql
+++ db/db-challenge3/db-010/010.sql
@@ -1,9 +1,9 @@
-SELECT U.user_name, MAX(C.created_at), CR.chatroom_name
+SELECT MAX(U.user_name), MAX(C.created_at), MAX(CR.chatroom_name)
 FROM users AS U
 LEFT JOIN chats AS C
 ON U.user_id = C.author_id
 LEFT JOIN chatrooms AS CR
 ON C.chatroom_id = CR.chatroom_id
 WHERE U.is_deleted = 0 AND CR. is_deleted = 0
-GROUP BY CR.chatroom_name
+GROUP BY CR.chatroom_id
 ORDER BY CR.chatroom_id
