7cf15ac1f6e774d3927e5cc96703bfa5924c07e3
川上登玄 <takazumi@takazumi-pc.local>
2020-06-16 18:35:04 +0900

    db-010に2パターン記述

--- db/db-challenge3/db-010/010.sql
+++ db/db-challenge3/db-010/010.sql
@@ -1,3 +1,15 @@
+SELECT MAX(U.user_name), MAX(C.created_at), CR.chatroom_name
+FROM users AS U
+LEFT JOIN chats AS C
+ON U.user_id = C.author_id
+LEFT JOIN chatrooms AS CR
+ON C.chatroom_id = CR.chatroom_id
+WHERE U.is_deleted = 0 AND CR. is_deleted = 0
+GROUP BY CR.chatroom_name,CR.chatroom_id
+ORDER BY CR.chatroom_id
+
+
+/* もしくはコチラ */
 SELECT MAX(U.user_name), MAX(C.created_at), MAX(CR.chatroom_name)
 FROM users AS U
 LEFT JOIN chats AS C
