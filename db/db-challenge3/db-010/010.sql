d94e80c1f93463e194ddfd685fed8ebdc977f6f0
川上登玄 <takazumi@takazumi-pc.local>
2020-06-17 17:33:25 +0900

    db-010を修正

--- db/db-challenge3/db-010/010.sql
+++ db/db-challenge3/db-010/010.sql
@@ -1,21 +1,36 @@
-SELECT MAX(U.user_name), MAX(C.created_at), CR.chatroom_name
+
+SELECT U.user_name, C.created_at, CR.chatroom_name
 FROM users AS U
-LEFT JOIN chats AS C
-ON U.user_id = C.author_id
-LEFT JOIN chatrooms AS CR
-ON C.chatroom_id = CR.chatroom_id
-WHERE U.is_deleted = 0 AND CR. is_deleted = 0
-GROUP BY CR.chatroom_name,CR.chatroom_id
-ORDER BY CR.chatroom_id
+JOIN chats AS C
+    ON U.user_id = C.author_id
+JOIN chatrooms AS CR
+    ON C.chatroom_id = CR.chatroom_id
+JOIN (
+    SELECT C2.chatroom_id, MAX(C2.created_at) AS ca
+    FROM chats AS C2
+    JOIN users AS U2
+        ON C2.author_id = U2.user_id
+    WHERE U2.is_deleted = 0 AND C2.is_deleted = 0
+    GROUP BY C2.chatroom_id
+) AS NEW
+ON CR.chatroom_id = NEW.chatroom_id AND C.created_at = NEW.ca
+WHERE C. is_deleted = 0 
 
 
-/* もしくはコチラ */
-SELECT MAX(U.user_name), MAX(C.created_at), MAX(CR.chatroom_name)
+--最新の投稿かどうかを、最終行のWHEREで判定
+SELECT U.user_name, C.created_at, CR.chatroom_name
 FROM users AS U
-LEFT JOIN chats AS C
-ON U.user_id = C.author_id
-LEFT JOIN chatrooms AS CR
-ON C.chatroom_id = CR.chatroom_id
-WHERE U.is_deleted = 0 AND CR. is_deleted = 0
-GROUP BY CR.chatroom_id
-ORDER BY CR.chatroom_id
+JOIN chats AS C
+    ON U.user_id = C.author_id
+JOIN chatrooms AS CR
+    ON C.chatroom_id = CR.chatroom_id
+JOIN (
+    SELECT C2.chatroom_id, MAX(C2.created_at) AS ca
+    FROM chats AS C2
+    JOIN users AS U2
+        ON C2.author_id = U2.user_id
+    WHERE U2.is_deleted = 0 AND C2.is_deleted = 0
+    GROUP BY C2.chatroom_id
+) AS NEW
+ON CR.chatroom_id = NEW.chatroom_id 
+WHERE C. is_deleted = 0 AND C.created_at = NEW.ca
